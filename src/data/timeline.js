c837604a4739c9755ee4574947a96642bdfa4836
saran <sarav200898@gmail.com>
2023-05-05 18:47:37 -0400

    added timeline data

---
+++ src/data/timeline.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        year : 'June 2023 to December 2023',
+        title : 'Software Engineer Co-op',
+        org : 'Klaviyo inc.',
+        details : 
+        'During my time as a Graduate Teaching Assistant at Northeastern University,  \
+        I provided guidance to 175 students in Object Oriented Design by conducting one-on-one office hours, \
+         assisting with class instruction, and grading assignments. \
+         I helped students understand Object Oriented Programming concepts,\
+          coding practices, and design patterns.', 
+    },
+    {
+        year : 'Jan 2023 to June 2023',
+        title : 'Graduate Teaching Assistant',
+        org : 'Northeastern University',
+        details : 
+        'During my time as a Graduate Teaching Assistant at Northeastern University,  \
+        I provided guidance to 175 students in Object Oriented Design by conducting one-on-one office hours, \
+         assisting with class instruction, and grading assignments. \
+         I helped students understand Object Oriented Programming concepts,\
+          coding practices, and design patterns.', 
+    },
+    {
+        year : 'April 2022 to August 2022',
+        title : 'Software Development Engineer II',
+        org : 'Ninjacart',
+        details : 
+        'During my time as a Graduate Teaching Assistant at Northeastern University,  \
+         I provided guidance to 175 students in Object Oriented Design by conducting one-on-one office hours, \
+         assisting with class instruction, and grading assignments. \
+         I helped students understand Object Oriented Programming concepts,\
+         coding practices, and design patterns.', 
+    }
+
+]
